d3067822d62bdc6c161f4c297aae7a65664668b0
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2022-06-02 15:19:13 +0300

    [feat] add model

---
+++ seq2seq.py
@@ -0,0 +1,60 @@
+import logging
+import pandas as pd
+from simpletransformers.seq2seq import Seq2SeqModel
+import argparse
+import torch
+
+def load_conllu_dataset(datafile):
+    arr = []
+    with open(datafile, encoding='utf-8') as inp:
+        strings = inp.readlines()
+    for s in strings:
+      if (s[0] != "#" and s.strip()):
+          split_string = s.split('\t')
+          arr.append([split_string[1] + " " + split_string[3]+ " " + split_string[5], split_string[2]])    
+    return pd.DataFrame(arr, columns=["input_text", "target_text"])
+
+def count_matches(labels, preds):
+    print(labels)
+    print(preds)
+    return sum([1 if label == pred else 0 for label, pred in zip(labels, preds)])
+
+def main(args):
+    train_df = load_conllu_dataset(args.train_data)
+    eval_df = load_conllu_dataset(args.dev_data)
+    model_args = {
+        "reprocess_input_data": True,
+        "overwrite_output_dir": True,
+        "max_seq_length": 10,
+        "train_batch_size": 8,
+        "num_train_epochs": 2,
+        "save_eval_checkpoints": False,
+        "save_model_every_epoch": False,
+        # "silent": True,
+        "evaluate_generated_text": False,
+        "evaluate_during_training": False,
+        "evaluate_during_training_verbose": False,
+        "use_multiprocessing": False,
+        "save_best_model": False,
+        "max_length": 15,
+        "save_steps": -1,
+    }
+    MODEL_PATH = "facebook/bart-large" if args.online != "0" else "bart-large"
+    model = Seq2SeqModel(
+        encoder_decoder_type="bart",
+        encoder_decoder_name=MODEL_PATH, 
+        args=model_args,
+	use_cuda = torch.cuda.is_available(),)    
+    model.train_model(train_df, eval_data=eval_df, matches=count_matches)
+    try:
+        print(model.eval_model(eval_df, matches=count_matches))
+    except:
+        print('Evaluation error')
+    
+if __name__ == '__main__':    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_data')
+    parser.add_argument('--dev_data')
+    parser.add_argument('--online', default="0")
+    args = parser.parse_args()
+    main(args)
